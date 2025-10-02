1feb542e0b24a4c9788bd1b756d82f58b811eb87
Mike Smith <m.t.smith@sheffield.ac.uk>
2025-10-02 09:03:49 +0100

    combine tool link

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
   keywords = ['image processing','retroreflectors'],
   classifiers = [],
   install_requires=['numpy'],
-  scripts=['bin/btretrodetect','bin/btretrodetect-train'],
+  scripts=['bin/btretrodetect','bin/btretrodetect-train','bin/btretrodetect-combine'],
 )
